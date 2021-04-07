eff51257a925ef327c4b5c18910cc157b0f63bd0
Desanti.Antoine <antoinedesanti@aol.fr>
2021-04-07 16:05:44 +0200

    instanciation de la navigation avec des fragments

--- appli/app/src/main/java/com/graillsain/graillsain/MainActivity.java
+++ appli/app/src/main/java/com/graillsain/graillsain/MainActivity.java
@@ -3,18 +3,65 @@ package com.graillsain.graillsain;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.NotificationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 
+import com.graillsain.graillsain.CartPage.CartFragment;
+import com.graillsain.graillsain.MapPage.MapFragment;
+import com.graillsain.graillsain.NewsPage.NewsFragment;
+import com.graillsain.graillsain.ProfilPage.ProfilFragment;
+import com.graillsain.graillsain.SearchPage.SearchFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     private int notificationId = 0;
 
-    private void sendNotificationOnChannel(String title, String content,
-                                           String channelID, int priority)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Fragment mapPageFragment = new MapFragment();
+        final Fragment profilPageFragment = new ProfilFragment();
+        final Fragment cartPageFragment = new CartFragment();
+        final Fragment searchPageFragment = new SearchFragment();
+        final Fragment newsPageFragment = new NewsFragment();
+
+
+
+
+        findViewById(R.id.menu_map_button).setOnClickListener(click -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, mapPageFragment).commit();
+           //  return@OnNavigationItemSelectedListener true
+        });
+        findViewById(R.id.menu_profil_button).setOnClickListener(click -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, profilPageFragment).commit();
+        });
+        findViewById(R.id.menu_cart_button).setOnClickListener(click -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, cartPageFragment).commit();
+        });
+
+        findViewById(R.id.menu_search_button).setOnClickListener(click -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, searchPageFragment).commit();
+        });
+        findViewById(R.id.menu_news_button).setOnClickListener(click -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, newsPageFragment).commit();
+        });
+
+
+
+
+
+    }
+
+    private void sendNotificationOnChannel(String title, String content, String channelID, int priority)
     {
         NotificationCompat.Builder notification = new NotificationCompat.Builder(this,
                 channelID)
@@ -25,18 +72,4 @@ public class MainActivity extends AppCompatActivity {
         NotificationManagerCompat.from(this).notify(notificationId, notification.build());
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.buttonNotify).setOnClickListener(click ->
-        {
-            Log.d("Notification", "Notification sent");
-            String title = ((EditText) findViewById(R.id.edit_text_title)).getText().toString();
-            String message = ((EditText) findViewById(R.id.edit_text_message)).getText().toString();
-            sendNotificationOnChannel(title, message, ApplicationDemo.CHANNEL_ID,
-                    NotificationCompat.PRIORITY_HIGH);
-        });
-    }
 }
---
+++ appli/app/src/main/java/com/graillsain/graillsain/MapPage/MapFragment.java
@@ -0,0 +1,23 @@
+package com.graillsain.graillsain.MapPage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.graillsain.graillsain.R;
+
+public class MapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.page_map, container, false);
+        return rootView;
+    }
+
+}
