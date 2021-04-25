b494be020e8f7adb88793ca7337a91cb4bd48671
Desanti.Antoine <antoinedesanti@aol.fr>
2021-04-25 22:57:42 +0200

    remove location references

--- appli/app/src/main/java/com/graillsain/graillsain/MapPage/MapFragment.java
+++ appli/app/src/main/java/com/graillsain/graillsain/MapPage/MapFragment.java
@@ -11,7 +11,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
-import com.graillsain.graillsain.Models.Location;
 import com.graillsain.graillsain.Models.Producer;
 import com.graillsain.graillsain.R;
 
