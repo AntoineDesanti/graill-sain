1d95e2754ff018fdb5cfc0b716f2ac1a08c65a1d
mathebrard <mathebrard47@gmail.com>
2021-04-27 21:22:26 +0200

    Structure producer page

--- appli/app/src/main/java/com/graillsain/graillsain/Models/Product.java
+++ appli/app/src/main/java/com/graillsain/graillsain/Models/Product.java
@@ -1,4 +1,27 @@
 package com.graillsain.graillsain.Models;
 
 public class Product {
+    private String name = "";
+    private double pricePerKilo = 0;
+
+    public Product(String name, double pricePerKilo){
+        this.name = name;
+        this.pricePerKilo = pricePerKilo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPricePerKilo() {
+        return pricePerKilo;
+    }
+
+    public void setPricePerKilo(double pricePerKilo) {
+        this.pricePerKilo = pricePerKilo;
+    }
 }
