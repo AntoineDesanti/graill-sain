6a022acb32caa1eb8ac37a214cbb509d3648d56b
mathebrard <mathebrard47@gmail.com>
2021-04-30 11:13:25 +0200

    Premier jet fonctionnel Page profil consommateur

--- appli/app/src/main/java/com/graillsain/graillsain/Models/Order.java
+++ appli/app/src/main/java/com/graillsain/graillsain/Models/Order.java
@@ -1,36 +1,19 @@
 package com.graillsain.graillsain.Models;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 public class Order {
     private int id;
     private ArrayList<Product> products;
-    private double total = 0;
-    private Date heurePassage = Calendar.getInstance().getTime();
-    private Consummer consummer;
+    private int total;
+    private Date heurePassage;
 
-    public Order(){
-
-    }
-
-    public Order(int id, ArrayList<Product> products, int total, Date heurePassage, Consummer conso) {
+    public Order(int id, ArrayList<Product> products, int total, Date heurePassage) {
         this.id = id;
         this.products = products;
         this.total = total;
         this.heurePassage = heurePassage;
-        this.consummer = conso;
-    }
-
-    public double countTotal(){
-        double somme = 0;
-        for (int i = 0; i<products.size(); i++)
-            somme += products.get(i).getPricePerKilo();
-        return somme;
     }
 
     public int getId() {
@@ -49,10 +32,7 @@ public class Order {
         this.products = products;
     }
 
-    public double getTotal() {
-        for ( Product p : products ) {
-            total += p.getPricePerKilo();
-        }
+    public int getTotal() {
         return total;
     }
 
@@ -66,17 +46,5 @@ public class Order {
 
     public void setHeurePassage(Date heurePassage) {
         this.heurePassage = heurePassage;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public Consummer getConsummer() {
-        return consummer;
-    }
-
-    public void setConsummer(Consummer consummer) {
-        this.consummer = consummer;
     }
 }
--- appli/app/src/main/java/com/graillsain/graillsain/ProfilPage/OrderAdapter.java
+++ appli/app/src/main/java/com/graillsain/graillsain/ProfilPage/OrderAdapter.java
@@ -1,12 +1,10 @@
 package com.graillsain.graillsain.ProfilPage;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,50 +14,45 @@ import com.graillsain.graillsain.Models.Order;
 import com.graillsain.graillsain.Models.Product;
 import com.graillsain.graillsain.R;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class OrderAdapter extends BaseAdapter {
+public class OrderAdapter {
 
-    private ArrayList<Order> orders;
+    private Order order;
     private LayoutInflater mInflater;
 
-    public OrderAdapter(Context context, ArrayList<Order> orders){
-        this.orders = orders;
+    public OrderAdapter(Context context, Order commande){
+        this.order = commande;
         this.mInflater = LayoutInflater.from(context);
     }
 
+    /*
     public int getCount(){
-        return orders.size();
+        return order.length();
     }
 
 
     public Object getItem(int position){
-        return orders;
+        return listProducts.get(position);
     }
 
     public long getItemId(int position){
         return position;
     }
 
+     */
 
-    @SuppressLint("SetTextI18n")
     public View getView(int position, View convertView, ViewGroup parent){
         ConstraintLayout layoutItem = (convertView == null ? (ConstraintLayout) mInflater.inflate(R.layout.order_consumer, parent, false) : (ConstraintLayout) convertView);
 
         TextView tvHeurePassage = layoutItem.findViewById(R.id.heure_passage);
         TextView tvOrder = layoutItem.findViewById(R.id.order);
 
-        //Log.d(new SimpleDateFormat()); //TODO: voir comment faire pour afficher la date correctemùent dans la ligne juste en-dessous
-
-        tvHeurePassage.setText("Heure de passage : " + orders.get(position).getHeurePassage().getHours()
-                + ":" + orders.get(position).getHeurePassage().getMinutes()
-                + "\n Total : " + orders.get(position).getTotal() + "0€"); //pour le moment réglé sur l'heure actuelle
-
-        String debutProduits = "";
-        for (int i=0; i<3; i++)
-            debutProduits += orders.get(position).getProducts().get(i).getName() + ", "; // pour l'affichage des produits dans la listview
-        tvOrder.setText("Panier #" + orders.get(position).getId() + "\n" + debutProduits);
+        //Log.d("Matthieu", "listProducts: " + order);
+        //Log.d("Matthieu", "product: " + order.getProducts().get(position));
+        //Log.d("Matthieu", "tvName: " + tvName);
+        tvHeurePassage.setText(order.getHeurePassage() + "");
+        tvOrder.setText("Panier " + order.getId());
 
         return layoutItem;
     }
