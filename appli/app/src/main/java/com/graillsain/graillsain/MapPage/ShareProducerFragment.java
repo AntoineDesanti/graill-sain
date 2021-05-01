047c0ce3397ce0b10fd6f092b477a790453a878b
Desanti.Antoine <antoinedesanti@aol.fr>
2021-05-01 23:38:15 +0200

    add a producer on the map

--- appli/app/src/main/java/com/graillsain/graillsain/MapPage/ShareProducerFragment.java
+++ appli/app/src/main/java/com/graillsain/graillsain/MapPage/ShareProducerFragment.java
@@ -2,25 +2,33 @@ package com.graillsain.graillsain.MapPage;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.graillsain.graillsain.Models.Producer;
 import com.graillsain.graillsain.R;
+
+import org.osmdroid.util.GeoPoint;
 
 
 public class ShareProducerFragment extends Fragment {
 
     private Bitmap producerPicture;
+    private GeoPoint currentLocation;
 
-    public ShareProducerFragment(Bitmap producerPicture) {
+    public ShareProducerFragment(Bitmap producerPicture, GeoPoint userLocation) {
         this.producerPicture = producerPicture;
+        currentLocation = userLocation;
     }
 
     @Nullable
@@ -33,6 +41,8 @@ public class ShareProducerFragment extends Fragment {
             getActivity().onBackPressed();
         });
 
+        ((TextView) rootView.findViewById(R.id.inputLocation)).setText(""+currentLocation.getLatitude()+" - "+currentLocation.getLongitude()+" 📍");
+
         ImageView capturedImage = rootView.findViewById(R.id.capturedImage);
         capturedImage.setImageBitmap(producerPicture);
 
@@ -44,8 +54,16 @@ public class ShareProducerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ((Button) view.findViewById(R.id.submit_button)).setOnClickListener(click -> {
+            submitSharingForm();
+        });
+    }
 
-
+    public void submitSharingForm(){
+        Producer.producers.add(
+                new Producer("Mon nouveau producteur", "333 Av ADAzf", currentLocation.getLatitude(),
+                currentLocation.getLongitude(),false));
+        getActivity().onBackPressed();
     }
 
 }
