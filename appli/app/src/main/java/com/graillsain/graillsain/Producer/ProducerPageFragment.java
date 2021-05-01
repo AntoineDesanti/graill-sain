84c747376b337a32c57e97323141f48a230ccdd6
Desanti.Antoine <antoinedesanti@aol.fr>
2021-05-01 19:25:00 +0200

    ProducerPage fragment + customOverlay with onClick listener

--- appli/app/src/main/java/com/graillsain/graillsain/Producer/ProducerPageFragment.java
+++ appli/app/src/main/java/com/graillsain/graillsain/Producer/ProducerPageFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -51,7 +52,8 @@ public class ProducerPageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.producer_page, container, false);
 
-
+        ((TextView) rootView.findViewById(R.id.nom_commerce)).setText(this.producer.getName());
+        ((TextView) rootView.findViewById(R.id.adresse_commerce)).setText(this.producer.getAddress());
 
         return rootView;
 
