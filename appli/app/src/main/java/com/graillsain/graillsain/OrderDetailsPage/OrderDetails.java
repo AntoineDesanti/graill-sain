341ad79faa387a4f77b05e6776cd200031a1b2dc
MathiasCARASCO <mathias.carasco@etu.unice.fr>
2021-05-02 12:43:57 +0200

    order details page

---
+++ appli/app/src/main/java/com/graillsain/graillsain/OrderDetailsPage/OrderDetails.java
@@ -0,0 +1,56 @@
+package com.graillsain.graillsain.OrderDetailsPage;
+
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+
+        import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
+        import androidx.fragment.app.Fragment;
+
+        import com.graillsain.graillsain.Models.Order;
+        import com.graillsain.graillsain.R;
+
+        import java.util.ArrayList;
+
+public class OrderDetails extends Fragment {
+    private Order order;
+
+    public OrderDetails(Order order) {
+        this.order = order;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.page_order_details, container, false);
+
+        ((TextView) rootView.findViewById(R.id.text_panier_id)).setText(this.order.getId());
+        ((SeekBar) rootView.findViewById(R.id.seekBar)).setProgress(1);
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+}
---
+++ appli/app/src/main/java/com/graillsain/graillsain/ProfilPage/OrdersFragment.java
@@ -0,0 +1,23 @@
+package com.graillsain.graillsain.ProfilPage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.graillsain.graillsain.R;
+
+class OrdersFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_orders, container, false);
+    }
+}
